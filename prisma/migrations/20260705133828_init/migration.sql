38ee8562054f4c6b1aba2550fcf66fe645da1a92
tanvir <tanvirislam173389@gmail.com>
2026-07-05 19:39:53 +0600

    added register user api

---
+++ prisma/migrations/20260705133828_init/migration.sql
@@ -0,0 +1,44 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('CUSTOMER', 'PROVIDER', 'ADMIN');
+
+-- CreateEnum
+CREATE TYPE "UserStatus" AS ENUM ('SUSPENDED', 'ACTIVE');
+
+-- CreateEnum
+CREATE TYPE "OrderStatus" AS ENUM ('CONFIRM', 'MARK_PICKED_UP', 'MARK_RETURNED');
+
+-- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_userId_fkey";
+
+-- DropTable
+DROP TABLE "Post";
+
+-- DropTable
+DROP TABLE "User";
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'CUSTOMER',
+    "status" "UserStatus" NOT NULL DEFAULT 'ACTIVE',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_id_key" ON "users"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
--- src/Models/Auth/auth.service.ts
+++ src/Models/Auth/auth.service.ts
@@ -1,5 +1,32 @@
-const createUserInDb = async (payload) => {
-    const { } = payload
+import { prisma } from "@/lib/prisma"
+import type { IUser } from "./user.interface"
+import bcrypt from 'bcrypt'
+
+const createUserInDb = async (payload: IUser) => {
+    const { email, password, name, role } = payload
+    const encryptedPassword = await bcrypt.hash(password, 10)
+
+    const userExsists = await prisma.user.findUnique({
+        where: {
+            email
+        }
+    })
+
+    if (userExsists) {
+        throw new Error("User already exists")
+    }
+
+    const cretateUser = await prisma.user.create({
+        data: {
+            email,
+            password: encryptedPassword,
+            name,
+            role
+        }
+    })
+
+    return cretateUser
+
 }
 
 const loginUserInDb = async () => {
